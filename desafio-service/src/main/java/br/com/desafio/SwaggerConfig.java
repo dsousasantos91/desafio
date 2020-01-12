d78d8fc3bd32413662975a2e136b425350a71669
Douglas de Sousa Santos <dsousasantos91@gmail.com>
2020-01-12 18:40:11 -0300

    Alterar nome pacote na documentação

--- desafio-service/src/main/java/br/com/desafio/SwaggerConfig.java
+++ desafio-service/src/main/java/br/com/desafio/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
 	public Docket apiConfigDocs() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.desafiocoopersystem.resource"))
+				.apis(RequestHandlerSelectors.basePackage("br.com.desafio.resource"))
 				.paths(PathSelectors.any())
 				.build();
 	}
