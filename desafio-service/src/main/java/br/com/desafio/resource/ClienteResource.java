bbd62a9e73ec41c0bc5a8ea367af9b87c6718b42
Douglas de Sousa Santos <dsousasantos91@gmail.com>
2020-01-12 18:38:37 -0300

    Adicionar regras de acesso aos recursos

--- desafio-service/src/main/java/br/com/desafio/resource/ClienteResource.java
+++ desafio-service/src/main/java/br/com/desafio/resource/ClienteResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +29,7 @@ public class ClienteResource {
 
 	@ApiOperation(value = "Listagem de clientes.")
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CLIENTE')")
 	public ResponseEntity<List<Cliente>> pesquisar() {
 		List<Cliente> clientes = pessoaService.pesquisar();
 		return ResponseEntity.ok(clientes);
@@ -35,6 +37,7 @@ public class ClienteResource {
 
 	@ApiOperation(value = "Informações sobre um cliente específico.")
 	@GetMapping(path = "/{id}", produces = "application/json")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CLIENTE')")
 	public ResponseEntity<Cliente> buscaoPorId(@PathVariable Long id) {
 		Cliente cliente = pessoaService.buscarPorId(id);
 		return ResponseEntity.ok(cliente);
@@ -42,6 +45,7 @@ public class ClienteResource {
 
 	@ApiOperation(value = "Cadastro de cliente.")
 	@PostMapping(produces = "application/json")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CLIENTE')")
 	public ResponseEntity<Cliente> salvar(@Valid @RequestBody Cliente cliente, HttpServletResponse response) {
 		Cliente clienteSalva = this.pessoaService.salvar(cliente);
 		publish.publishEvent(new RecursoCriadoEvent(this, response, cliente.getId()));
@@ -50,6 +54,7 @@ public class ClienteResource {
 
 	@ApiOperation(value = "Atualização de pessoa.")
 	@PutMapping(path = "/{id}", produces = "application/json")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CLIENTE')")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @Valid @RequestBody Cliente cliente) {
 		Cliente clienteSalva = this.pessoaService.atualizar(id, cliente);
 		return ResponseEntity.ok(clienteSalva);
@@ -58,6 +63,7 @@ public class ClienteResource {
 	@ApiOperation(value = "Remoção de pessoa e seus endereços.")
 	@DeleteMapping(path = "/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_CLIENTE')")
 	public void remover(@PathVariable Long id) {
 		this.pessoaService.remover(id);
 	}
